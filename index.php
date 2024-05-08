9130fdbc2646c04c378afcc37c932daf4b25e8d0
Tymek14695 <14695@eans-nt.edu.pl>
2024-05-08 11:15:43 +0200

    created repository

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pan Tadeusz</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <header>
+            <h1 class="text-center container">Pan Tadeusz, czyli ostatni zjazd na Litwie: historia szlachecka z 1811 i 1812 roku w dwunastu księgach wierszem pisana</h1>
+        </header>
+        <div class="row">
+            <div class="col">
+                <div class="list-group">
+                    <a class="list-group-item" href="./k1.html">Księga 1</a>
+                    <a class="list-group-item" href="./k2.html">Księga 2</a>
+                    <a class="list-group-item" href="./k3.html">Księga 3</a>
+                    <a class="list-group-item" href="./k4.html">Księga 4</a>
+                    <a class="list-group-item" href="./k5.html">Księga 5</a>
+                    <a class="list-group-item" href="./k6.html">Księga 6</a>
+                    <a class="list-group-item" href="./k7.html">Księga 7</a>
+                    <a class="list-group-item" href="./k8.html">Księga 8</a>
+                    <a class="list-group-item" href="./k9.html">Księga 9</a>
+                    <a class="list-group-item" href="./k10.html">Księga 10</a>
+                    <a class="list-group-item" href="./k11.html">Księga 11</a>
+                    <a class="list-group-item" href="./k12.html">Księga 12</a>
+                </div>
+            </div>
+            <div class="col-8">
+                <img src="tadek.jpg" class="img-fluid">
+            </div>
+        </div>
+        <footer>
+            <p class="text-center">©Tymoteusz Szarlej</p>
+        </footer>
+    </body>
+</html>
